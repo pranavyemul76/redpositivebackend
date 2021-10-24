e31f6a16418dce1320400e0676c4e1d3128bdbc6
pranavyemul76 <76551395+pranavyemul76@users.noreply.github.com>
2021-10-24 21:31:31 +0530

    Update Router.js

--- Router/Router.js
+++ Router/Router.js
@@ -6,4 +6,5 @@ routes.get('/userget',controllersignup.userdata);
 routes.post('/userpost',controllersignup.userpost);
 routes.delete('/deletedata/:email',controllersignup.userdelete);
 routes.put('/userupdate',controllersignup.userupdate);
+routes.post('/emailsend',controllersignup.sendemail);
 module.exports= routes;
